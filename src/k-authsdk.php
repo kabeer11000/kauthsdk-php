038a8e013fcb84247bfd286dc6403252dd7aa2a0
kabeer11000 <52799968+kabeer11000@users.noreply.github.com>
2020-08-21 23:22:00 +0500

    Kauth SDK Version 2

--- src/k-authsdk.php
+++ src/k-authsdk.php
@@ -1,35 +1,152 @@
 <?php
-class KAuth {
-private $actual_link = '';
-private $uniqueId = '';
-private $url = '';
-  function init ($r, $id, $m) {
-        function encrypt($string,$encryption_key){
-        
-            $ciphering = "AES-128-CTR";
-            $iv_length = openssl_cipher_iv_length($ciphering);
-            $options = 0;
-            $encryption_iv = '1234567891011121';
-            $encryption = openssl_encrypt($string, $ciphering,
-                $encryption_key, $options, $encryption_iv);
-            return $encryption;
-        }
-    $this->$uniqueId = urlencode(base64_encode(uniqid()));
-    $x = $this->$uniqueId;
-    $this->$actual_link = $r;
-    $this->$url = 'http://auth.kabeersnetwork.rf.gd/?redirect='.$this->$actual_link.'&clientId='.encrypt($id,$x).'&action='.$m.'&key='.$x;
-    }
-    function go(){
-      header('Location:'.$this->$url);      
-    }
-    function render($h, $w, $theme){
-        $unid = uniqid();
-        if($theme == 'dark'){
-            echo '<div class="k-net-login-btn-'.$unid.'"><a href="'.$this->$url.'"><img alt="Login With Kabeers Network" src="http://h8h7n5y3.hostrycdn.com/Private/uploads/df139cf745aa1942357bd354f4f00afae2445794k-btn.svg" style="width:'.$w.';height:'.$h.';"></a></div>';
-        }else{
-            echo '<div class="k-net-login-btn-'.$unid.'"><a href="'.$this->$url.'"><img alt="Login With Kabeers Network" src="http://h8h7n5y3.hostrycdn.com/Private/uploads/88deacec53d070c2db6ab659fcfb7bc87e473cfak-btn-light.svg" style="width:'.$w.';height:'.$h.';"></a></div>';
-        }
+declare(strict_types=1);
+namespace Kabeers;
+require 'small-http.php';
+
+use Error;
+use Exception;
+
+session_start();
+class KAuth
+{
+    public $tokens = null;
+    private $client_secret = null;
+    private $client_public = null;
+    private $save_dir = null;
+    private $auth_uri = null;
+    private $endPoints = array(
+        'UserInfo' => 'https://kabeers-auth.herokuapp.com/user/userinfo',
+        'AccessToken' => 'https://kabeers-auth.herokuapp.com/auth/token',
+        'RefreshToken' => 'https://kabeers-auth.herokuapp.com/auth/refresh',
+        'AuthURI' => 'https://kabeers-auth.herokuapp.com/auth'
+    );
+    private $session_state = false;
+
+    public function getUserInfo(String $token)
+    {
+        if (!$token || !$this->client_secret || !$this->client_public) return 0;
+        return preg_replace("/\s+/", "", SmallHttp::HTTPPost($this->endPoints['UserInfo'],
+            array(
+                "client_public" => "$this->client_public",
+                "client_secret" => "$this->client_secret",
+                "token" => "$token"
+            )
+        ));
     }
 
+    public function init(String $client_public, String $client_secret, String $save_dir, Bool $session_state = false)
+    {
+        if (!$client_public || !$client_secret || !$save_dir) return 0;
+        $this->client_public = $client_public;
+        $this->client_secret = $client_secret;
+        $this->save_dir = $save_dir;
+        if (isset($_GET['code'])) {
+            if ($session_state === false){
+                $token_response = json_decode($this->getAccessTokens(htmlspecialchars($_GET['code'])), true);
+                if ($token_response !== null && gettype($token_response) === 'array') {
+                    $this->tokens = $token_response;
+                    return true;
+                }
+            }else{
+                if (isset($_SESSION['kauth_state']) && $_GET['state']!==$_SESSION['kauth_state']) {return 0;}
+                $token_response = json_decode($this->getAccessTokens(htmlspecialchars($_GET['code'])), true);
+                if ($token_response !== null && gettype($token_response) === 'array') {
+                    $this->tokens = $token_response;
+                    return true;
+                }
+            }
+        }
+        return true;
+    }
+
+    public function refreshToken(String $refresh_token)
+    {
+        if (!$this->client_secret || !$this->client_public || !$refresh_token) return 0;
+        $jwt_payload = json_decode(base64_decode(urldecode(explode('.', $refresh_token)[1])));
+        if ($jwt_payload->iat > $jwt_payload->exp) {
+            throw new Error('Refresh Token Expired');
+        }
+        return SmallHttp::HTTPPost($this->endPoints['RefreshToken'],
+            array(
+                "client_public" => "$this->client_public",
+                "client_secret" => "$this->client_secret",
+                "refresh_token" => "$refresh_token"
+            ));
+    }
+
+    private function getAccessTokens(String $code)
+    {
+        if (!$this->client_secret || !$this->client_public || !$code) return 0;
+        return SmallHttp::HTTPPost($this->endPoints['AccessToken'],
+            array(
+                "client_public" => "$this->client_public",
+                "client_secret" => "$this->client_secret",
+                "auth_code" => "$code"
+            ));
+    }
+
+    public function saveToken(String $key, String $value)
+    {
+        if (!$value || !$key) return 0;
+        $return = false;
+        $key = md5($key);
+        if (isset($this->save_dir) && $this->save_dir !== '' || null) {
+            $save_key = fopen("$this->save_dir/$key.kauth_store", "w") or die("Unable to open file!");
+            fwrite($save_key, "$value");
+            fclose($save_key);
+            $return = true;
+        }
+        return $return;
+    }
+
+    public function getToken(String $key)
+    {
+        if (!$key) return 0;
+        $return = null;
+        $key = md5($key);
+        if (isset($this->save_dir) && $this->save_dir !== '' || null) {
+            $save_contents = null;
+            try {
+                $save_contents = file_get_contents("$this->save_dir/$key.kauth_store");
+                $return = true;
+            } catch (Exception $e) {
+                $return = false;
+            }
+            return $save_contents !== null || '' ? $save_contents : $return;
+        }
+        return true;
+    }
+
+    public function createAuthURI(Array $claims, String $callback, String $state, String $response_type = 'code', String $prompt = 'consent')
+    {
+        $callback = urlencode($callback);
+        $claims = urlencode(join(array_unique($claims), '|'));
+        if (!$state) {
+            $state = uniqid();
+        }
+        if (isset($this->session_state)){$_SESSION['kauth_state'] = $state;}
+        $endPoint = $this->endPoints['AuthURI'];
+        $this->auth_uri = "$endPoint/$this->client_public/$claims/$response_type/$callback/$state/$prompt";
+    }
+
+    public function render(String $height, String $width, String $theme = 'dark')
+    {
+        if (!$height || !$width || !$theme || !$this->auth_uri || $this->auth_uri === null) {
+            return false;
+        }
+        if ($theme === 'dark') {
+            return "<div class='kauth_btn--container'><a href='$this->auth_uri' class='kauth_btn--anchor'><img alt='Login With Kabeers Network' class='kauth_btn--image' src='https://cdn.jsdelivr.net/gh/kabeer11000/kauthsdk-php/dist/dark.svg' style='width:$width;height:$height'></a></div>";
+        }
+        return "<div class='kauth_btn--container'><a href='$this->auth_uri' class='kauth_btn--anchor'><img alt='Login With Kabeers Network' class='kauth_btn--image' src='https://cdn.jsdelivr.net/gh/kabeer11000/kauthsdk-php/dist/light.svg' style='width:$width;height:$height'></a></div>";
+    }
+
+    public function redirect()
+    {
+        if (!$this->auth_uri || $this->auth_uri === null) {
+            return false;
+        }
+        header("Location:$this->auth_uri");
+        return 0;
+    }
 }
 ?>
