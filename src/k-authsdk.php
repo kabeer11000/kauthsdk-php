865c0eca6f5bf7bfd5a789d57d20aa76757fa0ad
Kabeer Jaffri <52799968+kabeer11000@users.noreply.github.com>
2020-08-24 05:31:11 +0500

    Update k-authsdk.php

--- src/k-authsdk.php
+++ src/k-authsdk.php
@@ -7,21 +7,47 @@ use Error;
 use Exception;
 
 session_start();
+
 class KAuth
 {
+    /**
+     * @var null
+     */
     public $tokens = null;
+    /**
+     * @var null
+     */
     private $client_secret = null;
+    /**
+     * @var null
+     */
     private $client_public = null;
+    /**
+     * @var null
+     */
     private $save_dir = null;
+    /**
+     * @var null
+     */
     private $auth_uri = null;
+    /**
+     * @var array
+     */
     private $endPoints = array(
         'UserInfo' => 'https://kabeers-auth.herokuapp.com/user/userinfo',
         'AccessToken' => 'https://kabeers-auth.herokuapp.com/auth/token',
         'RefreshToken' => 'https://kabeers-auth.herokuapp.com/auth/refresh',
         'AuthURI' => 'https://kabeers-auth.herokuapp.com/auth'
     );
+    /**
+     * @var bool
+     */
     private $session_state = false;
 
+    /**
+     * @param String $token
+     * @return int|string|string[]|null
+     */
     public function getUserInfo(String $token)
     {
         if (!$token || !$this->client_secret || !$this->client_public) return 0;
@@ -34,6 +60,13 @@ class KAuth
         ));
     }
 
+    /**
+     * @param String $client_public
+     * @param String $client_secret
+     * @param String $save_dir
+     * @param bool $session_state
+     * @return bool|int|null
+     */
     public function init(String $client_public, String $client_secret, String $save_dir, Bool $session_state = false)
     {
         if (!$client_public || !$client_secret || !$save_dir) return 0;
@@ -41,31 +74,34 @@ class KAuth
         $this->client_secret = $client_secret;
         $this->save_dir = $save_dir;
         if (isset($_GET['code'])) {
-            if ($session_state === false){
+            if ($session_state === false) {
                 $token_response = json_decode($this->getAccessTokens(htmlspecialchars($_GET['code'])), true);
-                if ($token_response !== null && gettype($token_response) === 'array') {
-                    $this->tokens = $token_response;
-                    return true;
-                }
-            }else{
-                if (isset($_SESSION['kauth_state']) && $_GET['state']!==$_SESSION['kauth_state']) {return 0;}
+                $token_response !== null && gettype($token_response) === 'array' ? $this->tokens = $token_response : null;
+            } else {
+                if(isset($_SESSION['kauth_state']) && $_GET['state'] !== $_SESSION['kauth_state']) return false;
                 $token_response = json_decode($this->getAccessTokens(htmlspecialchars($_GET['code'])), true);
-                if ($token_response !== null && gettype($token_response) === 'array') {
-                    $this->tokens = $token_response;
-                    return true;
-                }
+                $token_response !== null && gettype($token_response) === 'array' ? $this->tokens = $token_response : null;
             }
+        }
+        if (isset($_GET['token'])) {
+            function objectToArray($d){if(is_object($d)){$d=get_object_vars($d);}if(is_array($d)){return array_map(__FUNCTION__,$d);}else{return $d;}}
+            $token_response = objectToArray(json_decode(urldecode($_GET['token'])));
+            if($session_state === false) return $token_response !== null || '' ? $this->tokens = $token_response : null;
+            if(isset($_SESSION['kauth_state']) && $_GET['state'] !== $_SESSION['kauth_state']) return 0;
+            return $token_response !== null || '' ? $this->tokens = $token_response : null;
         }
         return true;
     }
 
+    /**
+     * @param String $refresh_token
+     * @return bool|int|string
+     */
     public function refreshToken(String $refresh_token)
     {
         if (!$this->client_secret || !$this->client_public || !$refresh_token) return 0;
         $jwt_payload = json_decode(base64_decode(urldecode(explode('.', $refresh_token)[1])));
-        if ($jwt_payload->iat > $jwt_payload->exp) {
-            throw new Error('Refresh Token Expired');
-        }
+        if ($jwt_payload->iat > $jwt_payload->exp) throw new Error('Refresh Token Expired');
         return SmallHttp::HTTPPost($this->endPoints['RefreshToken'],
             array(
                 "client_public" => "$this->client_public",
@@ -74,6 +110,10 @@ class KAuth
             ));
     }
 
+    /**
+     * @param String $code
+     * @return bool|int|string
+     */
     private function getAccessTokens(String $code)
     {
         if (!$this->client_secret || !$this->client_public || !$code) return 0;
@@ -85,6 +125,24 @@ class KAuth
             ));
     }
 
+    /**
+     * @param $length
+     * @return string
+     */
+    private function genRandom($length) {
+        $char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_~";
+        $char = str_shuffle($char);
+        for($i = 0, $rand = '', $l = strlen($char) - 1; $i < $length; $i ++) {
+            $rand .= $char{mt_rand(0, $l)};
+        }
+        return $rand;
+    }
+
+    /**
+     * @param String $key
+     * @param String $value
+     * @return bool|int
+     */
     public function saveToken(String $key, String $value)
     {
         if (!$value || !$key) return 0;
@@ -99,6 +157,10 @@ class KAuth
         return $return;
     }
 
+    /**
+     * @param String $key
+     * @return bool|false|int|string|null
+     */
     public function getToken(String $key)
     {
         if (!$key) return 0;
@@ -116,51 +178,68 @@ class KAuth
         }
         return true;
     }
-    
-    public function deleteToken( String $key ){
+
+    /**
+     * @param String $key
+     * @return bool|int|null
+     */
+    public function deleteToken(String $key)
+    {
         if (!$key) return 0;
-        $return = null;
         $key = md5($key);
         if (isset($this->save_dir) && $this->save_dir !== null) {
             try {
                 unlink("$this->save_dir/$key.kauth_store");
-                $return = true;
+                return true;
             } catch (Exception $e) {
-                $return = false;
+                return false;
             }
-            return $return;
         }
         return true;
     }
 
-    public function createAuthURI(Array $claims, String $callback, String $state, String $response_type = 'code', String $prompt = 'consent')
+    /**
+     * @param array $claims
+     * @param String $callback
+     * @param String $state
+     * @param String $response_type
+     * @param String $prompt
+     * @param Int $nonce_state_length
+     */
+    public function createAuthURI(Array $claims, String $callback, String $state, String $response_type = 'code', String $prompt = 'consent', Int $nonce_state_length = 8)
     {
         $callback = urlencode($callback);
         $claims = urlencode(join(array_unique($claims), '|'));
-        if (!$state) {
-            $state = uniqid();
-        }
-        if (isset($this->session_state)){$_SESSION['kauth_state'] = $state;}
+        !$state || $state === '' || null ? $state = $this->genRandom($nonce_state_length) : null;
+        $nonce = $this->genRandom($nonce_state_length);
+        isset($this->session_state) ? $_SESSION['kauth_state'] = $state and $_SESSION['kauth_nonce'] = $nonce : null;
+        $response_type_token_extras = $response_type === 'token' ? '&'.http_build_query(array('client_secret'=>$this->client_secret)) : "";
         $endPoint = $this->endPoints['AuthURI'];
-        $this->auth_uri = "$endPoint/$this->client_public/$claims/$response_type/$callback/$state/$prompt";
+        $query = http_build_query(array("client_id" => $this->client_public, "scope" => $claims, "response_type" => $response_type, "redirect_uri" => urldecode($callback), "state" => $state, "nonce" => $nonce, "prompt" => $prompt)).$response_type_token_extras;
+        $this->auth_uri = "$endPoint/?$query";
     }
 
+    /**
+     * @param String $height
+     * @param String $width
+     * @param String $theme
+     * @return bool|string
+     */
     public function render(String $height, String $width, String $theme = 'dark')
     {
-        if (!$height || !$width || !$theme || !$this->auth_uri || $this->auth_uri === null) {
-            return false;
-        }
+        if (!$height || !$width || !$theme || !$this->auth_uri || $this->auth_uri === null) return false;
         if ($theme === 'dark') {
             return "<div class='kauth_btn--container'><a href='$this->auth_uri' class='kauth_btn--anchor'><img alt='Login With Kabeers Network' class='kauth_btn--image' src='https://cdn.jsdelivr.net/gh/kabeer11000/kauthsdk-php/dist/dark.svg' style='width:$width;height:$height'></a></div>";
         }
         return "<div class='kauth_btn--container'><a href='$this->auth_uri' class='kauth_btn--anchor'><img alt='Login With Kabeers Network' class='kauth_btn--image' src='https://cdn.jsdelivr.net/gh/kabeer11000/kauthsdk-php/dist/light.svg' style='width:$width;height:$height'></a></div>";
     }
 
+    /**
+     * @return bool|int
+     */
     public function redirect()
     {
-        if (!$this->auth_uri || $this->auth_uri === null) {
-            return false;
-        }
+        if (!$this->auth_uri || $this->auth_uri === null) return false;
         header("Location:$this->auth_uri");
         return 0;
     }
