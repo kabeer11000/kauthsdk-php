eac561234555ac3b310e61342000decddc4d347c
Kabeer Jaffri <52799968+kabeer11000@users.noreply.github.com>
2020-08-24 08:54:20 +0500

    Create k-authsdkTest.php

---
+++ src/k-authsdkTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Kabeers;
+
+use PHPUnit_Framework_TestCase;
+
+class KAuthTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testGetUserInfo()
+    {
+
+    }
+
+    public function testRender()
+    {
+
+    }
+
+    public function testRefreshToken()
+    {
+
+    }
+
+    public function testInit()
+    {
+
+    }
+
+    public function testSaveToken()
+    {
+
+    }
+
+    public function testGetToken()
+    {
+
+    }
+
+    public function testCreateAuthURI()
+    {
+
+    }
+}
